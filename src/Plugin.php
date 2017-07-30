0c94afd3dd638ec6b121b511ff87c3cb41059039
Joe Huss <detain@interserver.net>
2017-07-30 16:54:48 -0400

    various improvements

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,7 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 class Plugin {
 
 	public static $name = 'Google Plugin';
-	public static $description = 'Allows handling of Google emails and honeypots';
+	public static $description = 'Allows handling of Google based Analytics';
 	public static $help = '';
 	public static $type = 'plugin';
 
